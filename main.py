b3698b6a21af3cbaf8f998a1f577aeb36c9a1860
İclal Olukkaya <iclalolukkaya67@gmail.com>
2026-03-25 21:54:05 +0300

    Alt siniflar eklendi

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ class Kitap:
     def kitap_ekle(self):
         Kitap.kutuphane_envanteri.append(self)
         print("Kitap başarıyla eklendi.")
+
+        class BasiliKitap(Kitap):
+            def __init__(self, isim, yazar, sayfa_sayisi, tur, adet):
+                super().__init__(isim, yazar, sayfa_sayisi, tur)
+                self.adet = adet
+
+            def odunc_al(self):
+                if self.adet > 0:
+                    self.adet -= 1
+                    print(f"{self.isim} başarıyla ödünç alındı. Kalan adet: {self.adet}")
+                else:
+                    print(f"Maalesef {self.isim} adlı kitaptan stokta kalmadı.")
+
+            def iade_et(self):
+                self.adet += 1
+                print(f"{self.isim} başarıyla iade edildi. Güncel adet: {self.adet}")
+
+        class EKitap(Kitap):
+            def __init__(self, isim, yazar, sayfa_sayisi, tur, dosya_boyutu_mb):
+                super().__init__(isim, yazar, sayfa_sayisi, tur)
+                self.dosya_boyutu_mb = dosya_boyutu_mb
